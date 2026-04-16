a18cbf039deeded0d55a7af1e546cf07393b927b
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-16 11:40:39 +0200

    feat(infrastructure): add application db context

--- vertexERP.Infrastructure/Persistence/ApplicationDbContext.cs
+++ vertexERP.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,28 @@
-﻿namespace vertexERP.Infrastructure.Persistence
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using vertexERP.Application.Interfaces;
+using vertexERP.Domain.Modules.Auth;
+using vertexERP.Infrastructure.Identity;
+
+namespace vertexERP.Infrastructure.Persistence
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext :
+        IdentityDbContext<ApplicationUser, ApplicationRole, int>,
+        IApplicationDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<ApplicationRole> ApplicationRoles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+
     }
 }
