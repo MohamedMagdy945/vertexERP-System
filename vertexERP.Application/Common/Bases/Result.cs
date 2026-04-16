e327598b37f16f5767b34424e7caf65332e9b623
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-16 10:32:39 +0200

    feat(application): add result class

---
+++ vertexERP.Application/Common/Bases/Result.cs
@@ -0,0 +1,40 @@
+﻿namespace vertexERP.Application.Common.Bases
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string? Error { get; }
+
+        protected Result(bool isSuccess, string? error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success()
+            => new Result(true, null);
+
+        public static Result Failure(string error)
+            => new Result(false, error);
+    }
+
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T? Data { get; }
+        public string? Error { get; }
+
+        private Result(bool isSuccess, T? data, string? error)
+        {
+            IsSuccess = isSuccess;
+            Data = data;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value)
+            => new(true, value, null);
+
+        public static Result<T> Failure(string error)
+            => new(false, default, error);
+    }
+}
---
+++ vertexERP.Application/Common/Models/TokenResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace vertexERP.Application.Common.Models
+{
+    public class TokenResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+
+        public DateTime AccessTokenExpiration { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
