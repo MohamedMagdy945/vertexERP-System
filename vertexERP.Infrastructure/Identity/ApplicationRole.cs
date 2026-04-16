e217850eaa1f0900b344fdfb671ca81266be1d55
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-04-16 11:32:38 +0200

    feat(infrastructure): add application user and role

---
+++ vertexERP.Infrastructure/Identity/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace vertexERP.Infrastructure.Identity
+{
+    public class ApplicationRole : IdentityRole<int>
+    {
+
+    }
+}
---
+++ vertexERP.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using vertexERP.Domain.Modules.Auth;
+
+namespace vertexERP.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string? FirstName { get; set; }
+        public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    }
+}
